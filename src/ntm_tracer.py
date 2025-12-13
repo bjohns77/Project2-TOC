c6c7a3d44a65b0faa9acac5f4267b1fd525126fb
bjohns77 <brynn.l.johnson77@gmail.com>
2025-12-12 21:06:12 -0500

    fix the aplus case and code the ntm tracer

--- src/ntm_tracer.py
+++ src/ntm_tracer.py
@@ -22,33 +22,112 @@ class NTM_Tracer(TuringMachineSimulator):
         depth = 0
         accepted = False
 
+        #finding what the accepted and rejected states are:
+        accept_state = self.accept_state
+        reject_state = self.reject_state
+        states = self.states
+        trans = self.transitions
+        #for key, items in trans.items():
+         #   print(f"{key}: {items}")
+
+        #Turing machine configuration
         while depth < max_depth and not accepted:
             current_level = tree[-1]
-            next_level = []
+            next_level = [] # each config gets [left, state, right, [parent index, parent depth], transition]
             all_rejected = True
 
-            # TODO: STUDENT IMPLEMENTATION NEEDED
             # 1. Iterate through every config in current_level.
-            # 2. Check if config is Accept (Stop and print success) [cite: 179]
-            # 3. Check if config is Reject (Stop this branch only) [cite: 181]
-            # 4. If not Accept/Reject, find valid transitions in self.transitions.
-            # 5. If no explicit transition exists, treat as implicit Reject.
-            # 6. Generate children configurations and append to next_level[cite: 148].
+            for config in current_level:
+                state = ""
+                # 2. Check if config is Accept (Stop and print success) [cite: 179]
+                for item in config:
+                    if item in states: state = item # find state that it is in:
+                    if state == accept_state:
+                        print("Success!")
+                        accepted=True
+                        all_rejected=False
+                        self.print_trace_path(config, tree)
+                        break
+                    elif state == reject_state: break # 3. Check if config is Reject (Stop this branch only) [cite: 181]
+                if accepted: break # outer for loop breaks to while
+            
+                # find valid transitions:
+                if state in trans:
+                    past = config[0]
+                    next = config[2]
+                    if len(config[2])==0: next_input = '_'
+                    else: next_input = config[2][0] #look at next input
+                    s_transitions = trans[state]
+                    for index, t in enumerate(s_transitions):
+                        if next_input in t['read']:
+                            if t['move'][0] == 'R':
+                                new_left = past + t['write'][0]
+                                if not next:
+                                    new_right = '_'
+                                else:
+                                    new_right = next[1:]
+                                next_level.append([new_left, t['next'], new_right, [index, depth], t]) #Generate children configurations and append to next_level[cite: 148].
+                            elif t['move'][0] =='L':
+                                if not past:
+                                    new_left = ''
+                                    new_right = t['write'][0] + next
+                                else:
+                                    new_left = past[:-1]
+                                    new_right = past[-1] + t['write'][0] + next
+                                next_level.append([new_left, t['next'], new_right, [index, depth], t]) #Generate children configurations and append to next_level[cite: 148].
+                else: break # no transisiton off of the state so treat as reject 
+
 
             # Placeholder for logic:
+            # need to record maximum depth of rejected state:
+            deepest = depth
             if not next_level and all_rejected:
-                # TODO: Handle "String rejected" output [cite: 258]
+                # Handle "String rejected" output [cite: 258]
+                print("String Rejected!")
+                print(f"Longest Path: {deepest}")
                 break
-
+            #print(next_level)
             tree.append(next_level)
             depth += 1
 
         if depth >= max_depth:
             print(f"Execution stopped after {max_depth} steps.")  # [cite: 259]
 
-    def print_trace_path(self, final_node):
+    def print_trace_path(self, final_node, tree):
         """
         Backtrack and print the path from root to the accepting node.
         Ref: Section 4.2 [cite: 165]
         """
-        pass
+        path = []
+        curr_node = final_node
+        while len(curr_node) == 5:
+            # append node to path:
+            tree_index = curr_node[3][1]
+            curr_depth = tree_index+1
+            depth_index = curr_node[3][0]
+            
+            path.append({
+            'level': curr_depth,
+            'w_left': curr_node[0],  # Element 0: w_left
+            'state': curr_node[1],   # Element 1: state
+            'w_right': curr_node[2], # Element 2: w_right
+            })
+            #print(path)
+
+            curr_node = tree[tree_index][depth_index]
+        
+        #add root node:
+        path.append({
+            'level': 0,
+            'w_left': curr_node[0],  # Element 0: w_left
+            'state': curr_node[1],   # Element 1: state
+            'w_right': curr_node[2], # Element 2: w_right
+            })
+        
+        path.reverse() #sort from root to the end
+        
+        for node in path:
+            print(f"{node['w_left']}{node['state']}{node['w_right']}")
+
+        #print(path)
+        
